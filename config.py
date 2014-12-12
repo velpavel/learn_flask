6bfaf37dd8b09d171ef2d3bd446fa2b524f6a0ca
PaVel <vel.pavel@gmail.com>
2014-12-12 23:56:29 +0300

    Chapter 4

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 CSRF_ENABLED = True
 SECRET_KEY = 'Top-secret-key'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
